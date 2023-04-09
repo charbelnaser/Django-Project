1e9295b34d90f60fc04228b4950c8c9593a9c4c3
charbelnaser <nasr.charbel96@gmail.com>
2023-04-09 22:23:20 +0200

    committed successfully

---
+++ mysite/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from item.models import Item,Category
+# Create your views here.
+
+
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'main/index.html',{
+        'categories':categories,
+        'items':items,
+    })
+
+def contact(request):
+    return render(request, 'main/contact.html')
