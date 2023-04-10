4505fb0450272a1e7c1f0cb2c248bda1eb569fab
charbelnaser <nasr.charbel96@gmail.com>
2023-04-10 16:06:48 +0200

    commit 2

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from main.views import index,contact
+from main.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact ,name='contact'),
     path('admin/', admin.site.urls)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT )
